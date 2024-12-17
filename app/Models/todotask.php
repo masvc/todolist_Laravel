9cfe857fb8f71c7723a4d69d6bd1191f5421f4b8
mas <m.yoshida553@gmail.com>
2024-12-17 12:20:04 +0900

    tasktable made

---
+++ app/Models/todotask.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class todotask extends Model
+{
+    /** @use HasFactory<\Database\Factories\TodotaskFactory> */
+    use HasFactory;
+}
